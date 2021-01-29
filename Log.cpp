92ee35614830ff91496e6ea4064db58fb5a7cea8
Yuvraj883 <kingyuvi88@gmail.com>
2021-01-29 15:13:28 +0530

    Added namespace std

--- Log.cpp
+++ Log.cpp
@@ -1,4 +1,5 @@
 #include "Log.h"
+using namespace std; 
 
 void Log::SetLogLevel(int level){
         m_LogLevel = level;
@@ -6,18 +7,18 @@ void Log::SetLogLevel(int level){
 
 void Log::info(const char* msg){
         if(m_LogLevel >= LOG_INFO)
-            std::cout << "[Info]: " << msg << std::endl;
+            cout << "[Info]: " << msg << std::endl;
 }
 
 void Log::warn(const char* msg)
     {
         if(m_LogLevel >= LOG_WARN)
-            std::cout << "[Warning]: " << msg << std::endl;
+            cout << "[Warning]: " << msg << std::endl;
     }
 
 void Log::error(const char* msg)
     {
         if(m_LogLevel >= LOG_ERROR)
-            std::cout << "[Error]: " << msg << std::endl;
+            cout << "[Error]: " << msg << std::endl;
     }
 
